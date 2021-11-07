8f7a0fd45a026bf6a3c37dd58cb9368ec322467d
Anthony Kim <fbssaejun@gmail.com>
2021-11-07 14:33:15 -0500

    write second solution for anagram checker

--- anagramChecker.js
+++ anagramChecker.js
@@ -1,4 +1,7 @@
 
+
+/* ================================================== SOLUTION 1 ================================================================ */
+
 const anagramChecker = (str1, str2) => {
 
   //Remove any special characters, convert all letters to lowercase
@@ -13,4 +16,46 @@ const anagramChecker = (str1, str2) => {
 
 }
 
-console.log(anagramChecker("silent!!!!", "LisTen!"))
+console.log(anagramChecker("silent!!!!", "LisTen!")) // => Return true
+
+
+
+/* ================================================== SOLUTION 2 ================================================================ */
+
+//Helper function that creates a hashtable from given string
+const hashConverter = (str) => {
+  const obj = {}
+
+  for(let char of str) {
+    obj[char] = obj[char] + 1 || 1
+  }
+
+  return obj
+}
+
+console.log(hashConverter("anthony")) // Output: { a: 1, n: 2, t: 1, h: 1, o: 1, y: 1 }
+
+//Different solution using hash table
+const anagramChecker2 = (str1, str2) => {
+
+  //Remove any special characters, convert all letters to lowercase
+  str1 = str1.replace(/[^\w]/g, '').toLowerCase()
+  str2 = str2.replace(/[^\w]/g, '').toLowerCase()
+
+  const str1Map = hashConverter(str1)
+  const str2Map = hashConverter(str2)
+
+  for(let char in str1Map) {
+    if(str2Map[char] !== str1Map[char]) {
+      return false
+    }
+  }
+
+  return true;
+
+}
+
+console.log(anagramChecker2("silent", "listen!"))
+
+
+
