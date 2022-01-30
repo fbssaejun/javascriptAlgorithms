4952edf4a76e5d848aa35a67404887311f306d7e
Anthony Kim <fbssaejun@gmail.com>
2022-01-30 12:43:14 -0500

    create function that sums all prime numbers from a range of numbers

---
+++ sumPrimes.js
@@ -0,0 +1,32 @@
+/*
+
+QUESTION)
+
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+sumPrimes(10) should return 17.
+
+sumPrimes(977) should return 73156.
+
+*/
+
+const sumPrimes = (num) => {
+  let arr = [];
+  let counter = 0;
+  for(let i = 2; i <= num; i++) {
+    for(let j = 1; j <= i; j++) {
+      if(i % j === 0) {
+          counter += 1
+      }
+    }
+    if(counter === 2) {
+      arr.push(i)
+    }
+    counter = 0;
+  }
+  return arr.reduce((a,b) => a + b)
+}
+
+sumPrimes(10);
