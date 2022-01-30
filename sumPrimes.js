d48db9b5fec038039f3a2cacdeb9c9b406cd4efd
Anthony Kim <fbssaejun@gmail.com>
2022-01-30 12:47:04 -0500

    add comments to solution

--- sumPrimes.js
+++ sumPrimes.js
@@ -13,20 +13,27 @@ sumPrimes(977) should return 73156.
 */
 
 const sumPrimes = (num) => {
+  //Initialize empty array to find and store prime numbers
   let arr = [];
+  //Initialize counter variable to count how many times a given number is divisible inside the loop
   let counter = 0;
+  //Loop through num starting from 2, since 1 needs to be excluded
   for(let i = 2; i <= num; i++) {
+    //Loop through i to check how many times i is divisible
     for(let j = 1; j <= i; j++) {
       if(i % j === 0) {
           counter += 1
       }
     }
+    //if the given number (i) was only divisible two times, it means it is a prime number, push it to the empty array
     if(counter === 2) {
       arr.push(i)
     }
+    //Reset the counter
     counter = 0;
   }
+  //Sum all prime numbers in the array
   return arr.reduce((a,b) => a + b)
 }
 
-sumPrimes(10);
+sumPrimes(10); // => 17
