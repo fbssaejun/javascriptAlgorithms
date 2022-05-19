29667ba9ae516ce7fdc45ddb3a3663edabd8be08
Anthony Kim <fbssaejun@gmail.com>
2022-05-18 20:59:45 -0400

    create function that returns only truthly values from callback function throughout the input array

---
+++ dropIt.js
@@ -0,0 +1,19 @@
+/*
+
+QUESTION)
+
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+*/
+
+const dropElement = (arr, func) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) return arr.slice(i)
+  }
+  return []
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; }); // => [1,2]
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) // => [3, 4]
