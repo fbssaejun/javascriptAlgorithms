7fb8e71803986324f26e9bab5084cd47b2ee3de3
Anthony Kim <fbssaejun@gmail.com>
2022-05-23 23:50:27 -0400

    create object with getter and setter methods to set a person's name when contstructing a person object

---
+++ makeAPerson.js
@@ -0,0 +1,48 @@
+/* 
+
+QUESTION)
+
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+
+Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
+
+*/
+
+let Person = (firstAndLast) => {
+  let fullName = firstAndLast;
+
+  this.getFirstName = () => {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = () => {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = () => {
+    return fullName;
+  };
+
+  this.setFirstName = (first) => {
+    fullName = `${first} ${fullName.split(" ")[1]}`;
+  };
+
+  this.setLastName = (last) => {
+    fullName `${fullName.split(" ")[0]} ${last}`;
+  };
+
+  this.setFullName = (name) => {
+    fullName = name;
+  };
+};
+
+let bob = new Person("Bob Ross");
+
+bob.getFullName();  //  => "Bob  Ross"
