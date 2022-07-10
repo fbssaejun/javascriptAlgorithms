00e7bccc9e7e51affce21f40a3c114ea77f9d357
fbssaejun@gmail.com <fbssaejun@gmail.com>
2022-07-09 21:56:54 -0400

    find repeated characters using regex

---
+++ Regex/oneOrMoreCriminals.js
@@ -0,0 +1,12 @@
+/*
+
+QUESTION)
+
+Time to pause and test your new regex writing skills. A group of criminals escaped from jail and ran away, but you don't know how many. However, you do know that they stay close together when they are around other people. You are responsible for finding all of the criminals at once.
+
+Write a greedy regex that finds one or more criminals within a group of other people. A criminal is represented by the capital letter C.
+
+
+*/
+
+let reCriminals = /C+/g; // Change this line
